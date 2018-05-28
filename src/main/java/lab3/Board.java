320038f05af0d15d5de24e46fe7178abafd08ed9
mashapoli <mashapolichuk911@gmail.com>
2018-05-29 00:55:01 +0300

    Initial commit

---
+++ src/main/java/lab3/Board.java
@@ -0,0 +1,274 @@
+package lab3;
+
+public class Board {
+
+    public static final int EMPTY_SQUARE = 0;
+    public static final int WHITE_MEN_SQUARE = 1;
+    public static final int BLACK_MEN_SQUARE = -1;
+    public static final int WHITE_KING_SQUARE = 2;
+    public static final int BLACK_KING_SQUARE = -2;
+
+    // 0,0 - левый нижний; 0,7 - правый нижний; 7,0 - левый верхний
+    // белые внизу
+    int[][] squares;
+
+    public Board() {
+        this(new int[8][8]);
+        init();
+    }
+
+    public Board(int[][] squares) {
+        this.squares = squares;
+    }
+
+    void init() {
+
+        for (int rowIdx = 0; rowIdx < 8; rowIdx++) {
+            for (int colIdx = 0; colIdx < 8; colIdx++) {
+
+                if (rowIdx == 0 || rowIdx == 2) {
+                    if (colIdx % 2 == 0) {
+                        squares[rowIdx][colIdx] = WHITE_MEN_SQUARE;
+                    }
+                }
+                if (rowIdx == 1 && colIdx % 2 == 1) {
+                    squares[rowIdx][colIdx] = WHITE_MEN_SQUARE;
+                }
+                if (rowIdx == 5 || rowIdx == 7) {
+                    if (colIdx % 2 == 1) {
+                        squares[rowIdx][colIdx] = BLACK_MEN_SQUARE;
+                    }
+                }
+                if (rowIdx == 6 && colIdx % 2 == 0) {
+                    squares[rowIdx][colIdx] = BLACK_MEN_SQUARE;
+                }
+            }
+        }
+    }
+
+    public Board swap() {
+
+        int newSquares[][] = new int[8][8];
+
+        for (int rowIdx = 0; rowIdx < 8; rowIdx++) {
+            for (int colIdx = 0; colIdx < 8; colIdx++) {
+                newSquares[7 - rowIdx][7 - colIdx] = squares[rowIdx][colIdx] * -1;
+            }
+        }
+        System.out.println("Board swap");
+        return new Board(newSquares);
+    }
+
+
+    public int get(int rowIdx, int colIdx) {
+        return squares[rowIdx][colIdx];
+    }
+
+    public void set(int rowIdx, int colIdx, int value) {
+        squares[rowIdx][colIdx] = value;
+    }
+
+    public boolean isBoard(int rowIdx, int colIdx) {
+        return !(rowIdx < 0 || rowIdx > 7 || colIdx < 0 || colIdx > 7);
+    }
+
+    // только для белых
+    public boolean isPiece(int rowIdx, int colIdx) {
+        return isBoard(rowIdx, colIdx) && get(rowIdx, colIdx) > 0;
+    }
+
+    public boolean isEmpty(int rowIdx, int colIdx) {
+        return isBoard(rowIdx, colIdx) && get(rowIdx, colIdx) == 0;
+    }
+
+    public boolean isBlack(int rowIdx, int colIdx) {
+        return isBoard(rowIdx, colIdx) && get(rowIdx, colIdx) < 0;
+    }
+
+    public boolean isMenCapture(int rowIdx, int colIdx) {
+        if (isBoard(rowIdx, colIdx) && get(rowIdx, colIdx) == Board.WHITE_MEN_SQUARE) {
+            if (isBoard(rowIdx + 1, colIdx + 1) && get(rowIdx + 1, colIdx + 1) < 0
+                    && isBoard(rowIdx + 2, colIdx + 2) && get(rowIdx + 2, colIdx + 2) == 0
+                    || isBoard(rowIdx + 1, colIdx - 1) && get(rowIdx + 1, colIdx - 1) < 0
+                    && isBoard(rowIdx + 2, colIdx - 2) && get(rowIdx + 2, colIdx - 2) == 0
+                    || isBoard(rowIdx - 1, colIdx - 1) && get(rowIdx - 1, colIdx - 1) < 0
+                    && isBoard(rowIdx - 2, colIdx - 2) && get(rowIdx - 2, colIdx - 2) == 0
+                    || isBoard(rowIdx - 1, colIdx + 1) && get(rowIdx - 1, colIdx + 1) < 0
+                    && isBoard(rowIdx - 2, colIdx + 2) && get(rowIdx - 2, colIdx + 2) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isKingCapture(int rowIdx, int colIdx) {
+        if (isBoard(rowIdx, colIdx) && get(rowIdx, colIdx) == WHITE_KING_SQUARE) {
+            for (int blkIdx = 1; blkIdx < 8; blkIdx++) {
+                for (int stpIdx = blkIdx + 1; stpIdx < 8; stpIdx++) {
+
+                    if (isBoard(rowIdx + blkIdx, colIdx + blkIdx) && get(rowIdx + blkIdx, colIdx + blkIdx) < 0
+                            && isBoard(rowIdx + stpIdx, colIdx + stpIdx) && get(rowIdx + stpIdx, colIdx + stpIdx) == 0
+                            || isBoard(rowIdx + blkIdx, colIdx - blkIdx) && get(rowIdx + blkIdx, colIdx - blkIdx) < 0
+                            && isBoard(rowIdx + stpIdx, colIdx - stpIdx) && get(rowIdx + stpIdx, colIdx - stpIdx) == 0
+                            || isBoard(rowIdx - blkIdx, colIdx - blkIdx) && get(rowIdx - blkIdx, colIdx - blkIdx) < 0
+                            && isBoard(rowIdx - stpIdx, colIdx - stpIdx) && get(rowIdx - stpIdx, colIdx - stpIdx) == 0
+                            || isBoard(rowIdx - blkIdx, colIdx + blkIdx) && get(rowIdx - blkIdx, colIdx + blkIdx) < 0
+                            && isBoard(rowIdx - stpIdx, colIdx + stpIdx) && get(rowIdx - stpIdx, colIdx + stpIdx) == 0) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isCapture() {
+        for (int rowIdx = 0; rowIdx < 8; rowIdx++) {
+            for (int colIdx = 0; colIdx < 8; colIdx++) {
+                if (isMenCapture(rowIdx, colIdx) || isKingCapture(rowIdx, colIdx)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // только для белых
+    public boolean move(int fromRowIdx, int fromColIdx, int toRowIdx, int toColIdx) {
+
+        if (isBoard(fromRowIdx, fromColIdx) && get(fromRowIdx, fromColIdx) == WHITE_MEN_SQUARE
+                && isBoard(toRowIdx, toColIdx) && get(toRowIdx, toColIdx) == EMPTY_SQUARE
+                && Math.abs(fromRowIdx - toRowIdx) == 1 && Math.abs(fromColIdx - toColIdx) == 1
+                && toRowIdx > fromRowIdx) {
+
+            set(fromRowIdx, fromColIdx, EMPTY_SQUARE);
+            if (toRowIdx < 7) {
+                set(toRowIdx, toColIdx, WHITE_MEN_SQUARE);
+            } else {
+                set(toRowIdx, toColIdx, WHITE_KING_SQUARE);
+            }
+            System.out.println("move (" + fromRowIdx + "," + fromColIdx + ") to (" + toRowIdx + "," + toColIdx + ")");
+            return true;
+        }
+        if (isBoard(fromRowIdx, fromColIdx) && get(fromRowIdx, fromColIdx) == WHITE_KING_SQUARE
+                && isBoard(toRowIdx, toColIdx) && get(toRowIdx, toColIdx) == EMPTY_SQUARE) {
+
+            int rowDiff = toRowIdx - fromRowIdx;
+            int colDiff = toColIdx - fromColIdx;
+
+
+            if (Math.abs(rowDiff) == Math.abs(colDiff)) {
+
+                int stpRowStartIdx = rowDiff > 0 ? 1 : -1;
+                int stpRowInc = rowDiff > 0 ? 1 : -1;
+
+                int stpColStartIdx = colDiff > 0 ? 1 : -1;
+                int stpColInc = colDiff > 0 ? 1 : -1;
+
+                for (int stpRowIdx = fromRowIdx + stpRowStartIdx; stpRowIdx != fromRowIdx + rowDiff; stpRowIdx += stpRowInc) {
+                    for (int stpColIdx = fromColIdx + stpColStartIdx; stpColIdx != fromColIdx + colDiff; stpColIdx += stpColInc) {
+                        if (!(isBoard(stpRowIdx, stpColIdx) && get(stpRowIdx, stpColIdx) == EMPTY_SQUARE)) {
+                            return false;
+                        }
+                    }
+                }
+
+                set(fromRowIdx, fromColIdx, EMPTY_SQUARE);
+                set(toRowIdx, toColIdx, WHITE_KING_SQUARE);
+                System.out.println("move (" + fromRowIdx + "," + fromColIdx + ") to (" + toRowIdx + "," + toColIdx + ")");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // только для белых
+    public boolean capture(int fromRowIdx, int fromColIdx, int toRowIdx, int toColIdx) {
+
+        if (isMenCapture(fromRowIdx, fromColIdx)
+                && isBlack((fromRowIdx + toRowIdx) / 2, (fromColIdx + toColIdx) / 2)
+                && isEmpty(toRowIdx, toColIdx)) {
+
+            set(fromRowIdx, fromColIdx, EMPTY_SQUARE);
+            set((fromRowIdx + toRowIdx) / 2, (fromColIdx + toColIdx) / 2, EMPTY_SQUARE);
+            if (toRowIdx < 7) {
+                set(toRowIdx, toColIdx, WHITE_MEN_SQUARE);
+            } else {
+                set(toRowIdx, toColIdx, WHITE_KING_SQUARE);
+            }
+            System.out.println("capture (" + fromRowIdx + "," + fromColIdx + ") to (" + toRowIdx + "," + toColIdx + ")");
+            return true;
+        }
+
+        if (isKingCapture(fromRowIdx, fromColIdx)
+                && isEmpty(toRowIdx, toColIdx)) {
+
+            int rowDiff = toRowIdx - fromRowIdx;
+            int colDiff = toColIdx - fromColIdx;
+
+
+            if (Math.abs(rowDiff) == Math.abs(colDiff)) {
+
+                int stpRowStartIdx = rowDiff > 0 ? 1 : -1;
+                int stpRowInc = rowDiff > 0 ? 1 : -1;
+
+                int stpColStartIdx = colDiff > 0 ? 1 : -1;
+                int stpColInc = colDiff > 0 ? 1 : -1;
+
+                int blackCount = 0;
+                int blackRowIdx = -1, blackColIdx = -1;
+                for (int stpRowIdx = fromRowIdx + stpRowStartIdx; stpRowIdx != fromRowIdx + rowDiff; stpRowIdx += stpRowInc) {
+                    for (int stpColIdx = fromColIdx + stpColStartIdx; stpColIdx != fromColIdx + colDiff; stpColIdx += stpColInc) {
+                        if (isBoard(stpRowIdx, stpColIdx) && isBlack(stpRowIdx, stpColIdx)) {
+                            blackRowIdx = stpRowIdx;
+                            blackColIdx = stpColIdx;
+                            blackCount++;
+                        }
+                    }
+                }
+
+                if (blackCount != 1) {
+                    return false;
+                }
+
+                set(blackRowIdx, blackColIdx, EMPTY_SQUARE);
+                set(fromRowIdx, fromColIdx, EMPTY_SQUARE);
+                set(toRowIdx, toColIdx, WHITE_KING_SQUARE);
+                System.out.println("capture (" + fromRowIdx + "," + fromColIdx + ") to (" + toRowIdx + "," + toColIdx + ")");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void printBoard() {
+
+        for (int rowIdx = 7; rowIdx >= 0; rowIdx--) {
+            for (int colIdx = 0; colIdx < 8; colIdx++) {
+                String s;
+                switch (squares[rowIdx][colIdx]) {
+                    case Board.EMPTY_SQUARE:
+                        s = " ";
+                        break;
+                    case Board.BLACK_MEN_SQUARE:
+                        s = "B";
+                        break;
+                    case Board.BLACK_KING_SQUARE:
+                        s = "BK";
+                        break;
+                    case Board.WHITE_MEN_SQUARE:
+                        s = "W";
+                        break;
+                    case Board.WHITE_KING_SQUARE:
+                        s = "WK";
+                        break;
+                    default:
+                        throw new IllegalStateException();
+                }
+                System.out.print(s);
+                System.out.print("\t");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/lab3/GameFrame.java
@@ -0,0 +1,160 @@
+package lab3;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class GameFrame extends JFrame {
+
+    Board curBoard = new Board();
+
+    boolean isWhite = true;
+    Integer fromRowIdx, fromColIdx;
+    String msg = "";
+
+    public GameFrame() {
+        JPanel boardPanel = new JPanel() {
+            @Override
+            public void paint(Graphics g) {
+
+                // рисуем поле
+                for (int rowIdx = 0; rowIdx < 8; rowIdx++) {
+                    for (int colIdx = 0; colIdx < 8; colIdx++) {
+                        int x = colIdx * 64;
+                        int y = rowIdx * 64;
+
+                        if (rowIdx % 2 == colIdx % 2) {
+                            g.setColor(Color.LIGHT_GRAY);
+                        } else {
+                            g.setColor(Color.GRAY);
+                        }
+
+                        g.fillRect(x, y, 64, 64);
+                    }
+                }
+                if(!isWhite) {
+                    curBoard = curBoard.swap();
+                }
+                // рисуем фигуры
+                for (int rowIdx = 0; rowIdx < 8; rowIdx++) {
+                    for (int colIdx = 0; colIdx < 8; colIdx++) {
+
+                        int x = colIdx * 64;
+                        int y = rowIdx * 64;
+
+                        int square = curBoard.get(7 - rowIdx, colIdx);
+                        if (square != Board.EMPTY_SQUARE) {
+                            if (square > 0) {
+                                g.setColor(Color.WHITE);
+                            } else {
+                                g.setColor(Color.BLACK);
+                            }
+                            g.fillOval(x, y, 64, 64);
+
+                            if (Math.abs(square) == 2) {
+                                g.setColor(Color.BLUE);
+                                g.drawOval(x+8, y+8, 48, 48);
+                            }
+                        }
+                    }
+                }
+                if(!isWhite) {
+                    curBoard = curBoard.swap();
+                }
+                // рисуем чей ход
+                if(!isWhite) {
+                    g.setColor(Color.BLACK);
+                } else {
+                    g.setColor(Color.WHITE);
+                }
+                g.fillRect(0, 512, 512, 64);
+                g.setColor(Color.RED);
+                g.drawString(msg, 256, 530);
+            }
+        };
+        add(boardPanel);
+        boardPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int rowIdx = 7 - (e.getY() / 64);
+                int colIdx = e.getX() / 64;
+                action(rowIdx, colIdx);
+                repaint();
+            }
+        });
+    }
+
+    private boolean isFromIdxsSet() {
+        return fromRowIdx != null && fromColIdx != null;
+    }
+
+    private void unsetFromIdxs() {
+        fromRowIdx = null;
+        fromColIdx = null;
+    }
+
+    private void setFromIdxs(int fromRowIdx, int fromColIdx) {
+        this.fromRowIdx = fromRowIdx;
+        this.fromColIdx = fromColIdx;
+    }
+
+    private void action(int rowIdx, int colIdx) {
+
+        msg = "";
+
+        if(!isWhite) {
+            rowIdx = 7 - rowIdx;
+            colIdx = 7 - colIdx;
+        }
+
+        if(curBoard.isCapture() && !isFromIdxsSet()) {
+            if(curBoard.isMenCapture(rowIdx, colIdx)
+                || curBoard.isKingCapture(rowIdx, colIdx)) {
+                setFromIdxs(rowIdx, colIdx);
+            } else {
+                msg = "Ситуация \"бить обязан\"";
+            }
+        } else if (curBoard.isPiece(rowIdx, colIdx) && !isFromIdxsSet()) {
+            setFromIdxs(rowIdx, colIdx);
+        } else if (isFromIdxsSet()) {
+            if(curBoard.capture(fromRowIdx, fromColIdx, rowIdx, colIdx))  {
+                if(curBoard.isMenCapture(rowIdx, colIdx) || curBoard.isKingCapture(rowIdx, colIdx)) {
+                    setFromIdxs(rowIdx, colIdx);
+                } else {
+                    unsetFromIdxs();
+                    isWhite = !isWhite;
+                    curBoard = curBoard.swap();
+                }
+                return;
+            } else if(curBoard.move(fromRowIdx, fromColIdx, rowIdx, colIdx)) {
+                unsetFromIdxs();
+                isWhite = !isWhite;
+                curBoard = curBoard.swap();
+                return;
+            }
+            msg = "Некорректный ход";
+            unsetFromIdxs();
+            return;
+        }else {
+            msg = "Некорректный ход";
+            unsetFromIdxs();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                GameFrame gameFrame = new GameFrame();
+                Dimension size = new Dimension(520, 570);
+                gameFrame.setMinimumSize(size);
+                gameFrame.setMaximumSize(size);
+                gameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                gameFrame.setLocationRelativeTo(null);
+                gameFrame.pack();
+                gameFrame.setVisible(true);
+            }
+        });
+    }
+}
